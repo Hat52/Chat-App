c6d79cd2bda72f22e406407b3722bf36190b3858
hamza ali <tanolihamzaali@gmail.com>
2023-04-23 01:36:00 +0500

    client demo

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+app.use(cors());
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
 
 app.get('/hit', (req: any, res: any) => {
 	res.send('I have been hit');
 });
 
-app.listen(5000, () => console.log('Server is up and running'));
+const serverWithSocket = app.listen(3000, () => console.log('Server is up and running'));
+const io = new Server(serverWithSocket);
+
+io.on('connection', (socket: any) => {
+	console.log('here');
+	socket.on('chat message', (msg: any) => {
+		console.log('message: ' + msg);
+	});
+});
