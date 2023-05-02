c1043a35b20b27587d535fad699fdccb8bc35768
hamza ali <tanolihamzaali@gmail.com>
2023-05-03 01:12:15 +0500

    feat: socket connection established

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css';
+import { io } from 'socket.io-client';
 
 function App() {
+	const socket = io('http://localhost:5000');
+	socket.on('connect', () => {
+		console.log(`You have been connected by ID: ${socket.id}`);
+	});
+	socket.emit('test-message', 'Hello g this is a test message');
 	return <>Hello world</>;
 }
 
