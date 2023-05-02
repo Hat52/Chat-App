12121e23fc4330ec96a56ee6808b09be77e5aa64
hamza ali <tanolihamzaali@gmail.com>
2023-05-03 00:42:56 +0500

    Refactored structure

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// app.use(cors());
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require('socket.io');
+
+// app.get('/hit', (req: any, res: any) => {
+// 	res.send('I have been hit');
+// });
+
+// const serverWithSocket = app.listen(3000, () => console.log('Server is up and running'));
+// const io = new Server(serverWithSocket);
+
+// io.on('connection', (socket: any) => {
+// 	console.log('here');
+// 	socket.on('chat message', (msg: any) => {
+// 		console.log('message: ' + msg);
+// 	});
+// });
+
+const io = require('socket.io')(5000, {
+	cors: {
+		origin: ['*']
+	}
+});
+
+io.on('connection', (socket) => {
+	console.log(socket.id);
+});
