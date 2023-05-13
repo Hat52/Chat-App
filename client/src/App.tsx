41b4824d5e134bfef58b561da17691b1096ce636
hamza ali <tanolihamzaali@gmail.com>
2023-05-13 07:22:32 +0500

    chat header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from './components';
 function App() {
 	return (
 		<div className="flex h-screen justify-center items-center w-screen">
-			<div className="h-[90%] w-[80%] border-2 border-[#61BAF1] rounded-sm shadow-lg">
+			<div className="h-[90%] w-[80%] border-2 border-[#61BAF1] rounded-sm shadow-lg overflow-hidden">
 				<Sidebar />
 			</div>
 		</div>
--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -1,7 +1,9 @@
 export default function Sidebar() {
 	return (
-		<div className="w-1/4 bg-[#EFF3F950] h-full flex flex-col gap-2 px-2 py-5">
-			<h3>Chats</h3>
+		<div className="w-1/4 bg-[#EFF3F950] h-full flex flex-col gap-2">
+			<div className="bg-gray-100 h-[10%] flex justify-center items-center">
+				<h3 className="font-bold text-xl ">Chats</h3>
+			</div>
 		</div>
 	);
 }
