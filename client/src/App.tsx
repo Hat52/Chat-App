1bd47342368baa285f00044a78cd494555c4ea76
hamza ali <tanolihamzaali@gmail.com>
2023-05-13 10:13:52 +0500

    message preview container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css';
 import { Sidebar } from './components';
-import DemoPic from './assets/pic-demo.png';
 
 function App() {
 	return (
 		<div className="flex h-screen justify-center items-center w-screen">
 			<div className="h-[90%] w-[80%] border-2 border-[#61BAF1] rounded-sm shadow-lg overflow-hidden">
 				<Sidebar />
-			</div>
-			<div className="flex flex-col gap-2">
-				<div className="flex justify-between">
-					<img src={DemoPic} />
-				</div>
 			</div>
 		</div>
 	);
--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -1,8 +1,27 @@
+import DemoPic from '../../assets/pic-demo.png';
+
 export default function Sidebar() {
 	return (
 		<div className="w-1/4 bg-[#EFF3F950] h-full flex flex-col gap-2">
 			<div className="bg-gray-100 h-[10%] flex justify-center items-center">
 				<h3 className="font-bold text-xl ">Chats</h3>
+			</div>
+			<div className="flex flex-col gap-2">
+				<div className="flex justify-between items-center border border-gray-200 h-[56px] w-full px-2">
+					<div className="flex gap-2 items-center">
+						<img src={DemoPic} className="rounded-full w-[32px] h-[32px]" />
+						<div className="flex flex-col">
+							<h6 className="p-0 text-[14px] font-bold text-[#0000009B]">Hamza Ali</h6>
+							<p className="p-0 text-[12px] -mt-1">This is a new message</p>
+						</div>
+					</div>
+					<div className="flex flex-col items-center">
+						<p className="text-[12px] text-[#0000009B]">4:45PM</p>
+						<p className="rounded-full h-[18px] w-[18px] bg-[#62A1DD] text-[10px] text-white flex items-center justify-center">
+							1
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
