41a9456e316ec0e42509ee76fdb4238fcc6747c2
hamza ali <tanolihamzaali@gmail.com>
2023-05-14 20:53:02 +0500

    add: border

--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -4,7 +4,7 @@ import { MessageDummyData } from '../../constants';
 export default function Sidebar() {
 	return (
 		<div className="w-1/4 bg-[#EFF3F950] h-full flex flex-col gap-2">
-			<div className="bg-gray-100 h-[10%] flex justify-center items-center">
+			<div className="bg-gray-100 h-[10%] flex justify-center items-center border-r border-r-[#E1E5EA]">
 				<h3 className="font-bold text-xl ">Chats</h3>
 			</div>
 
