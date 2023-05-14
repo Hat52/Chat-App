ed142bbb4c6619117890f7f0d2fd69f889cfd27a
hamza ali <tanolihamzaali@gmail.com>
2023-05-14 20:48:13 +0500

    feat: Message container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import { Sidebar } from './components';
+import { Sidebar, MessageContainer } from './components';
 
 function App() {
 	return (
 		<div className="flex h-screen justify-center items-center w-screen">
-			<div className="h-[90%] w-[80%] border-2 border-[#61BAF1] rounded-sm shadow-lg overflow-hidden">
+			<div className="h-[90%] w-[80%] border-2 border-[#61BAF1] rounded-sm shadow-lg overflow-hidden flex">
 				<Sidebar />
+				<MessageContainer />
 			</div>
 		</div>
 	);
---
+++ client/src/components/MessageContainer/index.tsx
@@ -0,0 +1,3 @@
+export default function MessageContainer() {
+	return <h1>Message container</h1>;
+}
