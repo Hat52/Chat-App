ae8a79f82f7caa970bad377444a5da70f0a08ad4
hamza ali <tanolihamzaali@gmail.com>
2023-05-15 21:20:23 +0500

    new files

--- client/src/components/MessageContainer/index.tsx
+++ client/src/components/MessageContainer/index.tsx
@@ -1,3 +1,7 @@
+import Emoji from '../../assets/Emoji.svg';
+import Attachment from '../../assets/Attach.svg';
+import Mic from '../../assets/Mic.svg';
+
 interface IMessageContainer {
 	name: string;
 	avatar?: any;
@@ -5,7 +9,7 @@ interface IMessageContainer {
 
 export default function MessageContainer({ name, avatar }: IMessageContainer) {
 	return (
-		<div className="relative flex flex-col h-full w-full">
+		<div className="relative flex flex-col justify-between h-full w-[77%]">
 			<div className="h-[20%] bg-[#F0F4F9] w-full px-5 flex justify-center items-center gap-5 drop-shadow-md">
 				{avatar ? (
 					<img src={avatar} className="rounded-full w-[40px] h-[40px]" />
@@ -15,6 +19,20 @@ export default function MessageContainer({ name, avatar }: IMessageContainer) {
 					</div>
 				)}
 				<h1 className="font-semibold text-[28px] text-[#000000E4]">Hamza Ali</h1>
+			</div>
+			<div className="w-full h-[10%] flex items-center px-5">
+				<div className="relative w-full px-5 rounded-sm overflow-hidden bg-[#0000000A]">
+					<img
+						src={Attachment}
+						className="absolute left-2 top-1/2 transform -translate-y-1/2"
+					/>
+					<input
+						className="w-full h-[48px] px-5 bg-transparent"
+						value=""
+						placeholder="Write a message..."
+						onChange={() => {}}
+					/>
+				</div>
 			</div>
 		</div>
 	);
--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -2,7 +2,7 @@ import MessagePreview from '../MessagePreview';
 
 export default function Sidebar({ handleSelect, data }: any) {
 	return (
-		<div className="w-1/3 bg-[#EFF3F950] h-full flex flex-col gap-2">
+		<div className="w-[33%] bg-[#EFF3F950] h-full flex flex-col gap-2">
 			<div className="bg-gray-100 h-[10%] flex justify-center items-center border-r border-r-[#E1E5EA]">
 				<h3 className="font-bold text-xl ">Chats</h3>
 			</div>
