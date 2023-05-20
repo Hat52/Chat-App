76ba75a3012d56f457241ad083a646f3f667670a
hamza ali <tanolihamzaali@gmail.com>
2023-05-20 14:09:00 +0500

    Clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Sidebar, MessageContainer } from './components';
-import { MessageDummyData } from './constants';
 import { socket } from './socket';
 import DemoPic from './assets/pic-demo.png';
 
@@ -20,8 +19,6 @@ interface IMessages {
 
 function App() {
 	const [selectedMessage, setSelectedMessage] = useState(0);
-	const [isConnected, setIsConnected] = useState(socket.connected);
-	const [fooEvents, setFooEvents] = useState([]);
 	const [currentMessage, setCurrentMessage] = useState<IMessages[]>([
 		{
 			image: DemoPic,
@@ -37,38 +34,8 @@ function App() {
 		}
 	]);
 
-	useEffect(() => {
-		function onConnect() {
-			setIsConnected(true);
-		}
-
-		function onDisconnect(message: any) {
-			// setIsConnected(false);
-			console.log(message);
-		}
-
-		function onFooEvent(value: any) {
-			// setFooEvents(previous:any => [...previous, value]);
-		}
-
-		socket.on('connect', onConnect);
-		// socket.on('disconnect', onDisconnect);
-		// socket.on('foo', onFooEvent);
-
-		return () => {
-			socket.off('connect', onConnect);
-			socket.off('send-message', onDisconnect);
-			socket.off('foo', onFooEvent);
-		};
-	}, []);
-
 	const handleMessageSend = (message: string) => {
 		socket.emit('send-message', { message });
-		// setCurrentMessage((prevArray) => [
-		// 	...prevArray,
-		//
-		// ]);
-
 		const dummyCurrentMessage = [...currentMessage];
 		dummyCurrentMessage[selectedMessage].messages.push({ message, from: 'me', time: '9:45 PM' });
 		setCurrentMessage([...dummyCurrentMessage]);
