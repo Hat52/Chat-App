9330f499418cc55a9e79ec8b8a40c9889f99af2f
hamza ali <tanolihamzaali@gmail.com>
2023-05-20 13:55:23 +0500

    feat: onChange handler

--- client/src/components/MessageInput/index.tsx
+++ client/src/components/MessageInput/index.tsx
@@ -1,16 +1,31 @@
 import Emoji from '../../assets/Emoji.svg';
 import Attachment from '../../assets/Attach.svg';
 import Mic from '../../assets/Mic.svg';
+import { useState } from 'react';
 
-export default function MessageInput() {
+interface IMessageInput {
+	onChange: (message: string) => void;
+}
+
+export default function MessageInput({ onChange }: IMessageInput) {
+	const [currentMessage, setCurrentMessage] = useState<string>('');
+
+	const handleKeyDown = (e: any) => {
+		if (e.key === 'Enter') {
+			onChange(currentMessage);
+			setCurrentMessage('');
+		}
+	};
+
 	return (
 		<div className="relative w-full px-5 rounded-sm overflow-hidden bg-[#0000000A]">
 			<img src={Attachment} className="absolute left-2 top-1/2 transform -translate-y-1/2" />
 			<input
 				className="w-full h-[48px] px-5 bg-transparent outline-0"
-				value=""
 				placeholder="Write a message..."
-				onChange={() => {}}
+				onChange={(e) => setCurrentMessage(e.target.value)}
+				onKeyDown={handleKeyDown}
+				value={currentMessage}
 			/>
 			<div className="flex absolute right-2 gap-2 top-1/2 transform -translate-y-1/2">
 				<img src={Emoji} className="cursor-pointer" />
